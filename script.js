156a49654163c773a46ab7a312daf9041ee19c12
Alvaro <alvarocasillas237@gmail.com>
2025-09-04 20:19:40 +0200

    update72

--- script.js
+++ script.js
@@ -395,6 +395,6 @@ document.getElementById("computeOblique").addEventListener("click", computeObliq
   const toggle = document.querySelector(".menu-toggle");
   const nav = document.querySelector("nav");
 
-  toggle.addEventListener("click", () => {
-    nav.classList.toggle("mobile-open");
-  });
+document.querySelector('.menu-toggle').addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('mobile-open');
+});
